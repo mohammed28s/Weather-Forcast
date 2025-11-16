fe60a8eb4e7e8bfae9e01fe7fe48b511696c1182
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-11-16 14:12:50 +0400

    The depandcy has been removed

--- src/main/java/weather/com/Controller/WeatherController.java
+++ src/main/java/weather/com/Controller/WeatherController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import weather.com.DTO.WeatherResponse;
