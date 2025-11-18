a980aba6ec5f6267fcaae6fdfae3a04f3c4b7ff0
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-11-18 11:02:33 +0400

    The Weather fetching API is working will and tested

---
+++ RealGeoValuesForTesting.md
@@ -0,0 +1,27 @@
+
+These are real parameters or Query String for lat and lon
+
+Dubai:
+
+http://localhost:8080/api/weather?lat=25.2048&lon=55.2708
+
+
+
+
+Tokyo:
+
+http://localhost:8080/api/weather?lat=35.6895&lon=139.6917
+
+
+London:
+
+http://localhost:8080/api/weather?lat=51.5072&lon=-0.1276
+
+
+Seoul:
+
+http://localhost:8080/api/weather?lat=37.5665&lon=126.9780
+
+
+
+You can use each one of these to test the API
--- src/main/java/weather/com/Controller/WeatherController.java
+++ src/main/java/weather/com/Controller/WeatherController.java
@@ -7,16 +7,16 @@ package weather.com.Controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import weather.com.DTO.WeatherResponse;
 import weather.com.Service.WeatherService;
 
-@Service
-@RequestMapping("/api/weather")
-@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/api/weather")   // the main path to the weather APIs
 @Controller
 public class WeatherController {
 
@@ -24,8 +24,7 @@ public class WeatherController {
     private WeatherService weatherService;
 
 
-
-    @GetMapping("/temp")  // This is getting all the current Weather forecasting data
+    @GetMapping()  // This is getting all the current Weather forecasting data
     public WeatherResponse getWeather(@RequestParam Double lat, @RequestParam Double lon){
         return weatherService.getCurrentWeather(lat, lon);
     }
