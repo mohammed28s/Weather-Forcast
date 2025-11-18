15013c9723c4941380bb342b7e50c7ed07b1f5ec
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-11-18 10:40:35 +0400

    A New Java config file has been addded named: WeatherProperties

---
+++ src/main/java/weather/com/Configuration/WeatherProperties.java
@@ -0,0 +1,22 @@
+package weather.com.Configuration;
+
+
+
+
+
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "weather")
+@Data
+public class WeatherProperties {
+
+    private String baseUrl;
+    private String currentFields;
+
+
+}
