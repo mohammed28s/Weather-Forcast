34d7026e82dde5827e860c68a561c07a7bb78fdf
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-11-20 10:27:18 +0400

    The Weather API has been updated

--- src/main/java/weather/com/Controller/WeatherController.java
+++ src/main/java/weather/com/Controller/WeatherController.java
@@ -5,13 +5,16 @@ package weather.com.Controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import weather.ExceptionHandling.GlobalExceptionHandler;
 import weather.com.DTO.WeatherResponse;
 import weather.com.Service.WeatherService;
+
 
 
 
@@ -24,12 +27,23 @@ public class WeatherController {
     private WeatherService weatherService;
 
 
+    Logger logger = LoggerFactory.getLogger(WeatherController.class);
 
     @GetMapping("/current")  // This is getting all the current Weather forecasting data
     public WeatherResponse getWeather(@RequestParam Double lat, @RequestParam Double lon){
-        return weatherService.getCurrentWeather(lat, lon);
-    }
+        logger.trace("The Temp information has been fetched successfully");
+        logger.debug("The endpoint has been debug");
+        logger.info("This API fetching the latest Temp on the specific Geo location");
+        logger.warn("The Weather API is not working");
+        logger.error("The has issue fetching the Weather info or the parameters did not be add");
 
+        try {
+            return weatherService.getCurrentWeather(lat, lon);
+        } catch (Exception e) {
+            throw new GlobalExceptionHandler.ResourceNotFoundException("Add the lat and lon parameters");
+        }
+
+    }
 
 
 }
