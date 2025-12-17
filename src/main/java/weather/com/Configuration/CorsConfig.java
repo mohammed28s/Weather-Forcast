f7dfe2b6abc4a231cbc0e3983b96467f7dcde1b5
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-12-17 12:22:24 +0400

    The files has been updated

--- src/main/java/weather/com/Configuration/CorsConfig.java
+++ src/main/java/weather/com/Configuration/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {  // This is global CROS
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
