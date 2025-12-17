3a6d6347d09879f57e4af023a49f2d2ec3bc797a
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2025-12-17 08:13:11 +0400

    The RestClientConfig file has been updated

--- src/main/java/weather/com/Configuration/RestClientConfig.java
+++ src/main/java/weather/com/Configuration/RestClientConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class RestClientConfig {
+public class RestClientConfig {   // This is could be used for the restClient if needed
 
     @Bean
     public RestClient restClient() {
